c9f4417221e365ca900f084008010b56a0f4a4f3
João Vitor <joaovitorcoelho13@gmail.com>
2020-02-21 15:47:28 -0300

    add style home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
+
+function Product() {
+  return (
+    <li>
+    <img
+      src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ims=326x"
+      alt="tenis" />
+    <strong>Tenis Nike Revolution</strong>
+    <span>R$199,99</span>
+    <button type="button">
+      <div>
+        <MdAddShoppingCart size={16} color="#fff" /> 3
+      </div>
+      <span>ADICIONAR AO CARRINHO</span>
+    </button>
+  </li>
+  );
+}
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <ProductList>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </ProductList>
   );
 }
