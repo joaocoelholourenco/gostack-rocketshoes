248e76f3b319865ebe0e7a846ab83c424a01f22e
João Vitor <joaovitorcoelho13@gmail.com>
2020-02-24 14:03:12 -0300

    add config redux and combineReducers

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
+import { Provider } from 'react-redux';
+
 import Header from './components/Header';
 import Routes from './routes';
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <GlobalStyle />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <GlobalStyle />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
