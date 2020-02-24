42c10d94ff300b95b9315f9b5aafab86adf18341
João Vitor <joaovitorcoelho13@gmail.com>
2020-02-24 13:41:32 -0300

    get products api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-function Product() {
-  return (
-    <li>
-    <img
-      src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ims=326x"
-      alt="tenis" />
-    <strong>Tenis Nike Revolution</strong>
-    <span>R$199,99</span>
-    <button type="button">
-      <div>
-        <MdAddShoppingCart size={16} color="#fff" /> 3
-      </div>
-      <span>ADICIONAR AO CARRINHO</span>
-    </button>
-  </li>
-  );
-}
+export default class Home extends Component {
+  state = {
+    products: [],
+  }
+  async componentDidMount() {
+    const response = await api.get('products');
 
-export default function Home() {
-  return (
-    <ProductList>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-    </ProductList>
-  );
+    this.setState({
+      products: response.data
+    })
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img
+              src={product.image}
+              alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li >
+        ))}
+      </ProductList >
+    );
+  }
 }
